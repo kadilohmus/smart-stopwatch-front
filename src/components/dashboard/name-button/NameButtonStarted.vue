32b15f52ffc77bef407a87fcb697aaebe1d97bec
Kadi <kadilohmus@gmail.com>
2022-09-22 11:02:04 +0300

    SmartStopwatch front fix

--- src/components/dashboard/name-button/NameButtonStarted.vue
+++ src/components/dashboard/name-button/NameButtonStarted.vue
@@ -2,12 +2,16 @@
   <div>
     <button type="button" style="margin: 5px" class="btn btn-success btn-lg name-button-started"
             v-on:click="splitClick">
+        <span>{{ athleteEvent.athleteName }}</span>
+        <br>
 
-      <span>{{ athleteEvent.athleteName }}</span>
-      <br>
-      <span class="name-button-started-time">{{ distanceCoveredTime }}</span>
-      <br>
-      <span>{{ distanceCoveredLength }}m</span>
+      <div v-if="distanceCoveredTime != ''">
+        <span class="name-button-started-time">{{ distanceCoveredTime }}</span>
+        <br>
+      </div>
+      <div v-if="distanceCoveredTime != ''">
+        <span>{{ distanceCoveredLength }}m</span>
+      </div>
     </button>
     <font-awesome-icon icon="fa-solid fa-arrow-rotate-left" v-on:click="undoClick"/>
   </div>
@@ -62,17 +66,17 @@ export default {
       if (this.athleteEvent.lastSplitCount > 0) {
         var timeElapsed = new Date(this.athleteEvent.distanceCoveredTimeMilliseconds - this.athleteEvent.startTimeMilliseconds);
 
-      var min = timeElapsed.getUTCMinutes()
-      var sec = timeElapsed.getUTCSeconds()
-      var ms = timeElapsed.getUTCMilliseconds();
+        var min = timeElapsed.getUTCMinutes()
+        var sec = timeElapsed.getUTCSeconds()
+        var ms = timeElapsed.getUTCMilliseconds();
 
-      this.distanceCoveredTime =
-          // stopper 'hour' prefix is commented out
-          // remove comment "//" to enable hours
-          // this.zeroPrefix(hour, 1) + ":" +
-          this.zeroPrefix(min, 2) + ":" +
-          this.zeroPrefix(sec, 2) + "." +
-          this.zeroPrefix(ms, 2);
+        this.distanceCoveredTime =
+            // stopper 'hour' prefix is commented out
+            // remove comment "//" to enable hours
+            // this.zeroPrefix(hour, 1) + ":" +
+            this.zeroPrefix(min, 2) + ":" +
+            this.zeroPrefix(sec, 2) + "." +
+            this.zeroPrefix(ms, 2);
       }
     },
 
